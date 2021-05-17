57446996dccd721c6cdf91b271f31f764f3df5a9
Jan Brebeck <Jan.Brebeck98@gmail.com>
2021-05-17 21:39:34 +0200

    delted frontend

--- frontend/handler.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-col_names =  ['Title', 'Interpret', 'Genre', 'Lyrics', 'Happy', 'Angry', 'Surprise', 'Sad', 'Fear']
-
-df  = pd.DataFrame(columns = col_names)
-df.to_pickle('./../data/musicdata.pkl')
