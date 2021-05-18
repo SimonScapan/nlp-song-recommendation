5ded4bc0309a607e87822d13ed10e04e74c66de8
Jan Brebeck <Jan.Brebeck98@gmail.com>
2021-05-18 20:34:11 +0200

    Updates

---
+++ precache-manifest.3735774075b22ee106a04103fc50acd8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a2df7b9e9859e58dbac246b77d39334",
+    "url": "/nlp-song-recommendation/index.html"
+  },
+  {
+    "revision": "f043060f021b6ef091be",
+    "url": "/nlp-song-recommendation/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "f42ed8a38fcc6790be9d",
+    "url": "/nlp-song-recommendation/static/js/2.d7a1769d.chunk.js"
+  },
+  {
+    "revision": "7de21938117d0ee3a3c89357e2f4f400",
+    "url": "/nlp-song-recommendation/static/js/2.d7a1769d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f043060f021b6ef091be",
+    "url": "/nlp-song-recommendation/static/js/main.462f6be9.chunk.js"
+  },
+  {
+    "revision": "588b370048f1b77a8b7e",
+    "url": "/nlp-song-recommendation/static/js/runtime-main.aa18c490.js"
+  }
+]);
